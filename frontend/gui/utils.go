14ddff791e0e9cf2960153484e726c38a9f5e49b
Johannes Boehm <johannes.boehm1@swisscom.com>
2024-01-17 08:13:02 +0100

    handling not found error with empty data instead of error

--- frontend/gui/utils.go
+++ frontend/gui/utils.go
@@ -16,17 +16,22 @@ type backendData[t entities.Entity] struct {
 
 func requestData[t entities.Entity](url string) (*backendData[t], error) {
 	var data backendData[t]
+	fmt.Println("Requesting data from backend", url)
 	res, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
 	defer res.Body.Close()
 	if res.StatusCode != http.StatusOK {
+		if res.StatusCode == http.StatusNotFound {
+			fmt.Println("No data found")
+			return &data, nil
+		}
+		fmt.Println("Error with status code", res.StatusCode)
 		return nil, fmt.Errorf("error with status code %d from backend", res.StatusCode)
 	}
 	if err = json.NewDecoder(res.Body).Decode(&data); err != nil {
 		return nil, err
 	}
-
 	return &data, nil
 }
